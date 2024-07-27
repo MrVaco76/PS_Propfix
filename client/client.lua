0065f0fe483576ec30db78d1142e828ddfa6161f
PlexScripts <valerio.cola@gmail.com>
2024-07-27 16:59:48 +0200

    Notify added

--- client/client.lua
+++ client/client.lua
@@ -1,3 +1,20 @@
+
+if Config.Framework == "ESX" then
+    ESX = exports['es_extended']:getSharedObject()
+end
+
+-- Add a wait loop to ensure ESX is loaded
+if Config.Framework == "ESX" then
+    while not ESX do
+        Citizen.Wait(100)
+    end
+end
+
+if Config.Framework == "qb-core" then
+    QBCore = exports['qb-core']:GetCoreObject()
+end
+
+
 RegisterCommand(Config.PropFixCommand, function(source, args, rawCommand)
     local xPlayer = PlayerPedId()
     
@@ -33,7 +50,51 @@ RegisterCommand(Config.PropFixCommand, function(source, args, rawCommand)
 
     RemoveAttachedObjects(xPlayer)
 
+    if Config.SendNotification then 
+        local msg = Config.NotificationMsg
+        local type = "success"
+
+        Notify(msg, type)
+    end
     if Config.ResetTasks then
         ClearPedTasks(xPlayer)
     end
 end, false)
+
+
+
+function Notify(msg, type)
+    if Config.Notify == "ESX" then
+        if type == "info" then 
+            ESX.ShowNotification(msg, type, 5000)   
+        end
+        if type == "success" then
+            ESX.ShowNotification(msg, type, 5000)   
+        end
+        if type == "error" then
+            ESX.ShowNotification(msg, type, 5000)
+        end
+    elseif Config.Notify == "qb-core" then
+        if type == "primary" then 
+            QBCore.Functions.Notify(msg, "primary", 5000)
+        end
+        if type == "success" then
+            QBCore.Functions.Notify(msg, "success", 5000)
+        end
+        if type == "error" then
+            QBCore.Functions.Notify(msg, "error", 5000)
+        end
+    elseif Config.Notify == "standalone" then
+        ShowNotification(msg)
+    elseif Config.Notify == "custom" then
+        --add here your notify trigger
+    else
+        print("Unknown framework specified in Config.Framework!")
+    end
+end
+
+function ShowNotification(msg)
+    SetNotificationTextEntry("STRING")
+    AddTextComponentString(msg)
+    DrawNotification(false, true)
+end
--- config.lua
+++ config.lua
@@ -1,6 +1,9 @@
 Config = {}
 
 
-
+Config.Framework = "standalone" -- ESX / qb-core / standalone
 Config.PropFixCommand = "propfix" --Add here your command to delet the props.
 Config.ResetTasks = true  --If this is true, all animations will be canceled to.
+Config.SendNotification = true --If you want that the player get a notification then put this to ture
+Config.NotificationMsg = "All bugged props got deleted!" -- Add here your notification text that should displayed for your players
+Config.Notify = "standalone" --Change here you notifysystem qb-core / ESX / standalone / custom (for custom you need to add the trigger in the client.lua line 73)
